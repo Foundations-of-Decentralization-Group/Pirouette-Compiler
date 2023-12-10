cee706f101e113079fc08e0f047a7948927f41af
Prashant Godhwani <prashant.godhwani@hotmail.com>
2023-12-09 21:02:48 -0500

    Addressed Review comments

--- lib/ast_ml.ml
+++ lib/ast_ml.ml
@@ -1,96 +1,98 @@
-module rec S_P_TREE : sig
-  open Local_ast.L_P_TREE
+open Local_ast
 
-  type location = string
+type location = string
 
-  type tycon = string
+type tycon = string
 
-  and position = {
-    pos_fname : string;
-    pos_lnum : int;
-    pos_bol : int;
-    pos_cnum : int;
-  }
+type position = {
+  pos_fname : string;
+  pos_lnum : int;
+  pos_bol : int;
+  pos_cnum : int;
+}
 
-  and pattern =
-    | Left of pattern
-    | Right of pattern
-    | Pattern of (location * lpattern)
-    | Pair of (pattern * pattern)
-    | Var of var
-    | Wild
+type pattern =
+  | Left of pattern
+  | Right of pattern
+  | LocalPattern of (location * lpattern)
+  | Pair of (pattern * pattern)
+  | Var of var
+  | Wild
 
-  and sendWithBinder =  {
-    sender: location;
-    receiver: location;
-    binder: var;
-    expression: local_expr;
-    then_: choreography;
-  }
+and sendWithBinder =  {
+  sender: location;
+  receiver: location;
+  binder: var;
+  expression: local_expr;
+  then_: chor;
+}
 
-  and sendWithoutBinder = {
-    sender: location;
-    receiver: location;
-    choreo: choreography;
-  }
+and sendWithoutBinder = {
+  sender: location;
+  receiver: location;
+  choreo: chor;
+}
 
-  and syncData = {
-    sender: location;
-    receiver: location;
-    label: var;
-    then_: choreography;
-  }
+and syncData = {
+  sender: location;
+  receiver: location;
+  label: var;
+  then_: chor;
+}
 
-  and letData = {
-    declBlock: decl_block;
-    in_: choreography;
-  }
+and letData = {
+  declBlock: decl_block;
+  in_: chor;
+}
 
-  and matchData = {
-    matcher: choreography;
-    branches: (pattern * choreography) list;
-  }
+and matchData = {
+  matcher: chor;
+  branches: (pattern * chor) list;
+}
 
-  and pair = (choreography * choreography)
+and ifData = {
+  if_: chor;
+  then_: chor;
+  else_: chor;
+}
 
-  and choreography =
-    | Unit
-    | ChoreoVar of {data: var; position: position}
-    | Expression of {data: (location * local_expr); position: position}
-    | SendWithoutBinder of {data: sendWithoutBinder; position: position}
-    | SendWitBinder of {data: sendWithBinder; position: position}
-    | Branch of {data: (choreography * choreography * choreography); position: position}
-    | Sync of {data: syncData; position: position;}
-    | Let of {data: letData; position: position;}
-    | Pair of {data: pair; position: position}
-    | Fst of {data: pair; position: position}
-    | Snd of {data: pair; position: position}
-    | Match of {data: matchData; position: position;}
-    | Left of {data: matchData; position: position}
-    | Right of {data: matchData; position: position}
+and pair = (chor * chor)
 
-  and assignment =
-    | CType of {var: var; tycon: tycon}
-    | VarAsgn of {var: (var * tycon option); location: location option; choreo: choreography}
-    | Fun of {var: var; args: pattern list; choreo: choreography}
+and chor =
+  | Unit
+  | ChoreoVar of {data: var; position: position}
+  | Expression of {data: (location * local_expr); position: position}
+  | SendWithoutBinder of {data: sendWithoutBinder; position: position}
+  | SendWithBinder of {data: sendWithBinder; position: position}
+  | Branch of {data: ifData; position: position}
+  | Sync of {data: syncData; position: position;}
+  | Let of {data: letData; position: position;}
+  | Pair of {data: pair; position: position}
+  | Fst of {data: pair; position: position}
+  | Snd of {data: pair; position: position}
+  | Match of {data: matchData; position: position;}
+  | Left of {data: matchData; position: position}
+  | Right of {data: matchData; position: position}
 
-  and dec =
-    | FunDec of {var: var; tycons: tycon list}
-    | VarAsgnDec of {var: var; tycon: tycon; location: location option}
+and assignment =
+  | CType of {var: var; tycon: tycon}
+  | VarAsgn of {var: (var * tycon option); location: location option; choreo: chor}
+  | Fun of {var: var; args: pattern list; choreo: chor}
 
-  and exp_dec = dec
+and dec =
+  | FunDec of {var: var; tycons: tycon list}
+  | VarAsgnDec of {var: var; tycon: tycon; location: location option}
 
-  and decl_block_item = DecItem of dec | AssignmentItem of assignment
+and decl_block_item = DecItem of dec | AssignmentItem of assignment
 
-  and decl_block = T of {declBlockItems: decl_block_item list}
+and decl_block = T of {declBlockItems: decl_block_item list}
 
-  type program = T of {declBlock: decl_block}
+type program = T of {declBlock: decl_block}
 
-  (* let fileInfo = {
-    style = Control.style;
-    suffix = string;
-    display = t Control.display;
-  } *)
+(* let fileInfo = {
+  style = Control.style;
+  suffix = string;
+  display = t Control.display;
+} *)
 
-  (*Function toFile*)
-end = S_P_TREE
+(*Function toFile*)
--- lib/local_ast.ml
+++ lib/local_ast.ml
@@ -1,56 +1,54 @@
-module rec L_P_TREE : sig
-  type var = Var of string
+type var = Var of string
 
-  and value =
-    | Int of int
-    | String of string
-    | Bool of bool
+type value =
+  | Int of int
+  | String of string
+  | Bool of bool
 
-  and lpattern =
-    | Left of lpattern
-    | Right of lpattern
-    | Val of value
-    | Pair of (lpattern * lpattern)
-    | Var of var
-    | Wild
+type lpattern =
+  | Left of lpattern
+  | Right of lpattern
+  | Val of value
+  | Pair of (lpattern * lpattern)
+  | Var of var
+  | Wild
 
-  and local_assignment =
-    | LVarAsgn of { assign: (var * lpattern); expr: local_expr }
+and local_assignment =
+  | LVarAsgn of { assign: (var * lpattern); expr: local_expr }
 
-  and operation =
-    | Add of (local_expr * local_expr)
-    | Subtract of (local_expr * local_expr)
-    | Product of (local_expr * local_expr)
-    | Divide of (local_expr * local_expr)
-    | Lt of (local_expr * local_expr)
-    | Gt of (local_expr * local_expr)
-    | Lte of (local_expr * local_expr)
-    | Gte of (local_expr * local_expr)
-    | Eq of (local_expr * local_expr)
-    | Neq of (local_expr * local_expr)
-    | And of (local_expr * local_expr)
-    | Or of (local_expr * local_expr)
+and operation =
+  | Add of (local_expr * local_expr)
+  | Subtract of (local_expr * local_expr)
+  | Product of (local_expr * local_expr)
+  | Divide of (local_expr * local_expr)
+  | Lt of (local_expr * local_expr)
+  | Gt of (local_expr * local_expr)
+  | Lte of (local_expr * local_expr)
+  | Gte of (local_expr * local_expr)
+  | Eq of (local_expr * local_expr)
+  | Neq of (local_expr * local_expr)
+  | And of (local_expr * local_expr)
+  | Or of (local_expr * local_expr)
 
-  and pair = local_expr * local_expr
+and pair = local_expr * local_expr
 
-  and match_expr = {
-    matcher: match_expr;
-    branches: (lpattern * match_expr) list
-  }
+and match_expr = {
+  matcher: match_expr;
+  branches: (lpattern * match_expr) list
+}
 
-  and local_expr =
-    | Unit
-    | Val of value
-    | Var of var
-    | Binop of operation
-    | Let of {
-        assignment: local_assignment;
-        in_: local_expr
-      }
-    | Pair of pair
-    | Fst of pair
-    | Snd of pair
-    | Match of match_expr
-    | Left of match_expr
-    | Right of match_expr
-end = L_P_TREE
+and local_expr =
+  | Unit
+  | Val of value
+  | Var of var
+  | Binop of operation
+  | Let of {
+      assignment: local_assignment;
+      in_: local_expr
+    }
+  | Pair of pair
+  | Fst of pair
+  | Snd of pair
+  | Match of match_expr
+  | Left of match_expr
+  | Right of match_expr
