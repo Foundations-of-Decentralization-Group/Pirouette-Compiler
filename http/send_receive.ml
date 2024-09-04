d32f2112e90e441f8151bf7aa9ac9caf5e2f5866
Ethan Canton <ecanton@Ethans-MacBook-Pro-3.local>
2024-09-03 20:40:14 -0400

    Create prototype send/receive and config

---
+++ http/send_receive.ml
@@ -0,0 +1,57 @@
+open H2
+
+(* Send function *)
+let send_message connection message =
+  let request = 
+    Request.create
+      `POST
+      "/"
+      ~scheme:"https"
+      ~headers:(Headers.of_list [
+        ":authority", "compiler.example.com";
+        "content-type", "text/plain";
+        "content-length", string_of_int (String.length message)
+      ])
+  in
+  let request_body = Client.request connection request ~error_handler:(fun _ -> ()) ~response_handler:(fun _ _ -> ()) in
+  Body.Writer.write_string request_body message;
+  Body.Writer.close request_body
+
+(* Receive function *)
+let receive_message connection =
+  let response_received, notify_response_received = Promise.create () in
+  let response_handler response response_body =
+    match response.Response.status with
+    | `OK ->
+      let buffer = Buffer.create 1024 in
+      let rec read_response () =
+        Body.Reader.schedule_read
+          response_body
+          ~on_read:(fun bigstring ~off ~len ->
+            let chunk = Bigstringaf.substring bigstring ~off ~len in
+            Buffer.add_string buffer chunk;
+            read_response ())
+          ~on_eof:(fun () ->
+            Promise.fulfill notify_response_received (Buffer.contents buffer))
+      in
+      read_response ()
+    | _ ->
+      Printf.eprintf "Unexpected response status: %s\n" 
+        (Status.to_string response.Response.status);
+      Promise.fulfill notify_response_received "Error: Unexpected response status"
+  in
+  let error_handler error =
+    Printf.eprintf "Error: %s\n" (H2.Error_code.to_string error);
+    Promise.fulfill notify_response_received "Error: Connection error"
+  in
+  let request = 
+    Request.create
+      `GET
+      "/"
+      ~scheme:"https"
+      ~headers:(Headers.of_list [":authority", "compiler.example.com"])
+  in
+  let _ = Client.request connection request ~error_handler ~response_handler in
+  Promise.await response_received
+
+
