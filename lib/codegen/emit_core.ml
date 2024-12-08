772a5ad7b80e79d4fb3ddf7abedd1758dc055274
Ethan Canton <cethan988@gmail.com>
2024-12-07 21:42:40 -0500

    Makes dune file

--- lib/codegen/emit_core.ml
+++ lib/codegen/emit_core.ml
@@ -172,10 +172,6 @@ and emit_net_pexp ~(self_id : string) (module Msg : Msg_intf) (exp : 'a Net.expr
     in
     Ast_builder.Default.pexp_match ~loc (emit_net_pexp ~self_id (module Msg) e) cases
   | Send (e, LocId (dst, _), _) ->
-    (* Msg.emit_net_send
-       ~src:self_id
-       ~dst
-       [%expr Marshal.to_string [%e emit_net_pexp ~self_id (module Msg) e] []] *)
     let val_id = Id.gen "val_" in
     [%expr
       let [%p Ast_builder.Default.pvar ~loc val_id] =
@@ -187,7 +183,10 @@ and emit_net_pexp ~(self_id : string) (module Msg : Msg_intf) (exp : 'a Net.expr
           ~dst
           [%expr Marshal.to_string [%e Ast_builder.Default.evar ~loc val_id] []]]]
   | Recv (LocId (src, _), _) ->
-    [%expr Marshal.from_string [%e Msg.emit_net_recv ~src ~dst:self_id] 0]
+    [%expr 
+      Marshal.from_string 
+        ([%e Msg.emit_net_recv ~src ~dst:self_id]) 
+        0]
   | ChooseFor (LabelId (label, _), LocId (dst, _), e, _) ->
     Ast_builder.Default.esequence
       ~loc
