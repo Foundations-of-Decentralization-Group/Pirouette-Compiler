5603118973efab29a3ac66bd031f70a2f81cbfd2
Julia Montuori <juliamontuori@Julias-MacBook-Pro-182.local>
2025-03-06 22:36:42 -0500

    test ast_core int string and bool set and get value

--- test/core_test.ml
+++ test/core_test.ml
@@ -1,41 +1,26 @@
 open OUnit2
 open Ast_core
-open Local.M
 
-(* open M *)
-
-(* module DummyInfo = struct
-  type t = 'a 
-end *)
-
-(* module With (Info : sig
-  type t
-end) =
-struct
-type nonrec value = Info.t M.value
-type nonrec loc_id = Info.t M.loc_id
-type nonrec var_id = Info.t M.var_id
-type nonrec typ_id = Info.t M.typ_id
-type nonrec sync_label = Info.t M.sync_label
-type nonrec un_op = Info.t M.un_op
-type nonrec bin_op = Info.t M.bin_op
-type nonrec typ = Info.t M.typ
-type nonrec pattern = Info.t M.pattern
-type nonrec expr = Info.t M.expr*)
-
-(* module LocalAst = Local.With()  *)
 module DummyInfo = struct
   type t = int
 end
 
 module LocalAst = Local.With(DummyInfo)
-
-
-(* let test_change_string (old_string: string) (new_string: string) =
-    let old_info = Local.M.String(old_string, old_string) in
-    let new_info = LocalAst.set_info_value new_string old_info in
-    assert_equal new_string (LocalAst.get_info_value (new_info))
+(* let test_type_bool (new_meta: int) (old_meta: int) (input_bool: bool) =
+  let old_info = Local.M.TBool(input_bool, old_meta) in
+  let new_info = LocalAst.set_info_typ new_meta (old_info) in
+  assert_equal new_meta (LocalAst.get_info_typ (new_info))
 ;; *)
+let test_change_bool (new_meta: int) (old_meta: int) (input_bool: bool) =
+  let old_info = Local.M.Bool(input_bool, old_meta) in
+  let new_info = LocalAst.set_info_value new_meta old_info in
+  assert_equal new_meta (LocalAst.get_info_value (new_info))
+;;
+let test_change_string (new_meta: int) (old_meta: int) (input_string: string) =
+    let old_info = Local.M.String(input_string, old_meta) in
+    let new_info = LocalAst.set_info_value new_meta old_info in
+    assert_equal new_meta (LocalAst.get_info_value (new_info))
+;;
 
 let test_change_int (old_int:'a) (new_int:'a) =
   let old_info = Local.M.Int(old_int, old_int) in
@@ -54,13 +39,20 @@ let suite =
    ("test_simple" >:: fun _ -> test_change_int 10 20);
     ("test_simple" >:: fun _ -> test_change_int (-1) (-2));
     ("test_simple" >:: fun _ -> test_change_int 1000 2000);
-  (* ];"string Tests" 
+  ];"string Tests" 
   >::: [
-  ("test_simple" >:: fun _ -> test_change_string ("hi") ("hello"));
-  ("test_simple" >:: fun _ -> test_change_string 2 3);
- ("test_simple" >:: fun _ -> test_change_string 10 20);
-  ("test_simple" >:: fun _ -> test_change_string (-1) (-2));
-  ("test_simple" >:: fun _ -> test_change_string 1000 2000); *)
+  ("test_simple" >:: fun _ -> test_change_string 1 2 ("hello"));
+  ("test_simple" >:: fun _ -> test_change_string 2 3 ("hellonjdvsbvjsbgjkbs"));
+  ("test_simple" >:: fun _ -> test_change_string 10 20 ("hi"));
+  ("test_simple" >:: fun _ -> test_change_string 1000 2000 ("hello"));
+  ("test_simple" >:: fun _ -> test_change_string (-1) (-2) ("hello"));
+];"bool Tests" 
+>::: [
+("test_simple" >:: fun _ -> test_change_bool 1 2 (false));
+("test_simple" >:: fun _ -> test_change_bool 2 3 (true));
+("test_simple" >:: fun _ -> test_change_bool 10 20 (false));
+("test_simple" >:: fun _ -> test_change_bool 1000 2000 (true));
+("test_simple" >:: fun _ -> test_change_bool (-1) (-2) (false));
 ]
   ]
 
