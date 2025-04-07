566930c85b45642c358ed73fb955226fc0d407e8
Yining Zhang <zhang482@buffalo.edu>
2025-04-07 02:40:12 -0400

    refactor msg_intf and toplevel

--- lib/codegen/msg_intf.ml
+++ lib/codegen/msg_intf.ml
@@ -1,40 +1,6 @@
 open Ppxlib
 
-let spf = Printf.sprintf
-let loc = { !Ast_helper.default_loc with loc_ghost = true }
-
 module type M = sig
-  val emit_toplevel_defs : string list -> value_binding list
   val emit_net_send : src:string -> dst:string -> expression -> expression
   val emit_net_recv : src:string -> dst:string -> expression
-end
-
-module Msg_chan_intf : M = struct
-  let emit_toplevel_defs loc_ids =
-    let loc_pairs =
-      List.concat_map
-        (fun a -> List.filter_map (fun b -> if a <> b then Some (a, b) else None) loc_ids)
-        loc_ids
-    in
-    List.map
-      (fun (a, b) ->
-        Ast_builder.Default.value_binding
-          ~loc
-          ~pat:(Ast_builder.Default.pvar ~loc (spf "chan_%s_%s" a b))
-          ~expr:[%expr Domainslib.Chan.make_bounded 0])
-      loc_pairs
-  ;;
-
-  let emit_net_send ~src ~dst pexp =
-    ignore dst;
-    [%expr
-      Domainslib.Chan.send
-        [%e Ast_builder.Default.evar ~loc (spf "chan_%s_%s" src dst)]
-        [%e pexp]]
-  ;;
-
-  let emit_net_recv ~src ~dst =
-    [%expr
-      Domainslib.Chan.recv [%e Ast_builder.Default.evar ~loc (spf "chan_%s_%s" src dst)]]
-  ;;
 end
--- lib/codegen/toplevel_domain.ml
+++ lib/codegen/toplevel_domain.ml
@@ -5,22 +5,36 @@ open Ppxlib
 let spf = Printf.sprintf
 let loc = { !Ast_helper.default_loc with loc_ghost = true }
 
-let emit_toplevel_shm
-  out_chan
-  (module Msg : Msg_intf.M)
-  (loc_ids : string list)
-  (net_stmtblock_l : 'a Net.stmt_block list)
+module Msg_chan_intf : Msg_intf.M = struct
+  let emit_net_send ~src ~dst pexp =
+    ignore dst;
+    [%expr
+      Domainslib.Chan.send
+        [%e Ast_builder.Default.evar ~loc (spf "chan_%s_%s" src dst)]
+        [%e pexp]]
+  ;;
+
+  let emit_net_recv ~src ~dst =
+    [%expr
+      Domainslib.Chan.recv [%e Ast_builder.Default.evar ~loc (spf "chan_%s_%s" src dst)]]
+  ;;
+end
+
+let emit_toplevel_domain
+      out_chan
+      (loc_ids : string list)
+      (net_stmtblock_l : 'a Net.stmt_block list)
   =
   let emit_domain_bindings
-    (loc_ids : string list)
-    (net_stmtblock_l : 'a Net.stmt_block list)
+        (loc_ids : string list)
+        (net_stmtblock_l : 'a Net.stmt_block list)
     =
     let main_expr = ref (Ast_builder.Default.eunit ~loc) in
     let rec emit_net_toplevel loc_id stmts =
       match stmts with
       | [] -> !main_expr
       | stmt :: stmts ->
-        (match Emit_core.emit_net_binding ~self_id:loc_id (module Msg) stmt with
+        (match Emit_core.emit_net_binding ~self_id:loc_id (module Msg_chan_intf) stmt with
          | exception Emit_core.Main_expr e ->
            main_expr := e;
            emit_net_toplevel loc_id stmts
@@ -33,10 +47,10 @@ let emit_toplevel_shm
     in
     List.map2
       (fun loc_id net_stmts ->
-        Ast_builder.Default.value_binding
-          ~loc
-          ~pat:(Ast_builder.Default.pvar ~loc (spf "domain_%s" loc_id))
-          ~expr:[%expr Domain.spawn (fun _ -> [%e emit_net_toplevel loc_id net_stmts])])
+         Ast_builder.Default.value_binding
+           ~loc
+           ~pat:(Ast_builder.Default.pvar ~loc (spf "domain_%s" loc_id))
+           ~expr:[%expr Domain.spawn (fun _ -> [%e emit_net_toplevel loc_id net_stmts])])
       loc_ids
       net_stmtblock_l
   in
@@ -57,12 +71,26 @@ let emit_toplevel_shm
       (emit_domain_bindings loc_ids net_stmtblock_l)
       (emit_domain_join_seq loc_ids)
   in
+  let emit_chan_defs loc_ids =
+    let loc_pairs =
+      List.concat_map
+        (fun a -> List.filter_map (fun b -> if a <> b then Some (a, b) else None) loc_ids)
+        loc_ids
+    in
+    List.map
+      (fun (a, b) ->
+         Ast_builder.Default.value_binding
+           ~loc
+           ~pat:(Ast_builder.Default.pvar ~loc (spf "chan_%s_%s" a b))
+           ~expr:[%expr Domainslib.Chan.make_bounded 0])
+      loc_pairs
+  in
   let ppf = Format.formatter_of_out_channel out_chan in
   Pprintast.structure
     ppf
     [ Ast_builder.Default.pstr_eval
         ~loc
-        (match Msg.emit_toplevel_defs loc_ids with
+        (match emit_chan_defs loc_ids with
          | [] -> domain_body (* When no initialization needed *)
          | bindings -> Ast_builder.Default.pexp_let ~loc Nonrecursive bindings domain_body)
         []
