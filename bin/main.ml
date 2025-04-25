69b544424dd6b9e1015e7e6dcf5865b5f80bf67b
Ethan Canton <cethan988@gmail.com>
2025-04-25 00:42:12 -0400

    FFI works

--- bin/main.ml
+++ bin/main.ml
@@ -23,41 +23,98 @@ let speclist =
 ;;
 
 (* Helper function to generate the dune file *)
-let generate_dune_file base_name locs backend =
+let generate_dune_file base_name locs backend netir_l =
+  (* Extract FFI libraries from all network ASTs *)
+  let ffi_libs = 
+    List.fold_left (fun acc ir ->
+      let files = Ast_utils.collect_ffi_files ir in
+      List.fold_left (fun acc' file -> file :: acc') acc files
+    ) [] netir_l
+    |> List.sort_uniq String.compare
+    |> List.map Filename.basename
+    |> List.map (fun file -> 
+       (* Remove .ml extension for module names in dune file *)
+       if Filename.check_suffix file ".ml" then
+         Filename.remove_extension file
+       else 
+         file)
+  in
+  
   let common_libs = "ast_core parsing codegen ast_utils netgen ppxlib" in
-  let dune_path, dune_content =
-    match backend with
-    | "shm" ->
-      let content = Printf.sprintf
-        {|(executable
+  (* Add FFI libraries if they exist *)
+  let library_deps = 
+    if List.length ffi_libs > 0 then
+      common_libs ^ " " ^ String.concat " " ffi_libs
+    else
+      common_libs
+  in
+  
+  match backend with
+  | "shm" ->
+    (* For SHM backend, create a new dune file in the root *)
+    let content = Printf.sprintf
+      {|(executable
  (name %s)
  (libraries %s domainslib))
 |}
-        base_name (* Use the extracted base name *)
-        common_libs
-      in
-      ("dune", content) (* Output dune file to the root for shm *)
+      base_name (* Use the extracted base name *)
+      library_deps
+    in
+    let oc = open_out "dune" in
+    output_string oc content;
+    close_out oc
 
-    | "http" ->
-      let executable_names =
-        List.map (fun loc -> Printf.sprintf "%s_%s" base_name loc) locs (* Use the extracted base name *)
-        |> String.concat " "
-      in
-      let content = Printf.sprintf
-        {|(executables
- (names %s)
- (libraries %s http_pirc lwt cohttp-lwt-unix yojson))
+  | "http" ->
+    (* For HTTP backend, create a new dune file in examples/ directory *)
+    let dune_path = "examples/dune" in
+    let executable_names =
+      List.map (fun loc -> Printf.sprintf "%s_%s" base_name loc) locs (* Use the extracted base name *)
+      |> String.concat " "
+    in
+      
+    (* Create library section for FFI files if they exist *)
+    let lib_section = 
+      if List.length ffi_libs > 0 then
+        Printf.sprintf
+          {|(library
+ (name ffi_lib)
+ (modules %s)
+ (libraries %s))
 |}
-        executable_names
-        common_libs
-      in
-      (Filename.concat "examples" "dune", content) (* Output dune file to examples/ for http *)
+          (String.concat " " ffi_libs)
+          common_libs
+      else
+        Printf.sprintf
+          {|(library
+ (name ffi_lib)
+ (modules)
+ (libraries %s))
+|}
+          common_libs
+    in
+      
+    (* Create executables section *)
+    let exec_section = Printf.sprintf
+      {|(executables
+ (names %s)
+ (modules %s)
+ (libraries %s ffi_lib http_pirc lwt cohttp-lwt-unix yojson)
+ (flags (:standard -w -26)))
+|}
+      executable_names
+      executable_names  (* Use executable names as module names too *)
+      common_libs
+    in
+      
+    (* Create complete dune file content *)
+    let final_content = lib_section ^ "\n" ^ exec_section in
+      
+    (* Write the content to the dune file, overwriting any existing file *)
+    let oc = open_out dune_path in
+    output_string oc final_content;
+    close_out oc
 
-    | _ -> invalid_arg "Invalid backend for dune generation"
-  in
-  let oc = open_out dune_path in
-  output_string oc dune_content;
-  close_out oc
+  | _ -> invalid_arg "Invalid backend for dune generation"
 ;;
 
 let () =
@@ -111,13 +168,29 @@ let () =
     if not (Sys.file_exists examples_dir && Sys.is_directory examples_dir) then
       Unix.mkdir examples_dir 0o755;
 
+    (* Extract FFI libraries from all network ASTs *)
+    let ffi_libs = 
+      List.fold_left (fun acc ir ->
+        let files = Ast_utils.collect_ffi_files ir in
+        List.fold_left (fun acc' file -> file :: acc') acc files
+      ) [] netir_l
+      |> List.sort_uniq String.compare
+    in
+    
+    (* Determine if we have any FFI files *)
+    let has_ffi_files = List.length ffi_libs > 0 in
+
     List.iter2
       (fun loc ir ->
         (* Use the extracted base name here *)
         let ml_filename = Printf.sprintf "%s_%s.ml" !basename loc in
         let out_path = Filename.concat examples_dir ml_filename in (* Output to examples/ *)
         let out_file = open_out out_path in
-        output_string out_file "open Http_pirc\n\n";
+        (* Only add open Ffi_lib if FFI files are used *)
+        if has_ffi_files then
+          output_string out_file "open Http_pirc\nopen Ffi_lib\n\n"
+        else
+          output_string out_file "open Http_pirc\n\n";
         Codegen.Toplevel_shm.emit_toplevel_http out_file msg_module [ loc ] [ ir ];
         close_out out_file)
       locs
@@ -125,5 +198,5 @@ let () =
   | _ -> invalid_arg "Invalid backend");
 
   (* Generate the dune file after generating all ml files *)
-  generate_dune_file !basename locs !backend
+  generate_dune_file !basename locs !backend netir_l
 ;;
