e2f8e1b558997451a6f3039fb629008f31eaa46b
zhartov <magusshulem@gmail.com>
2025-08-19 19:15:56 -0400

    Added the optimization possible flag to the list that contains the strings for all the participants

--- lib/ast_utils/sync_optimize.ml
+++ lib/ast_utils/sync_optimize.ml
@@ -26,48 +26,51 @@ and copy_matcher_stmt
   match x with
   | Assign (_, e, _) ->
     (* this is where we go to the choreo expr variant*)
-    let rs_list = copy_choreo_expr e holder1 _holder2 in
+    let rs_list = visit_choreo_expr e holder1 _holder2 in
     rs_list
   | _ ->
     print_endline "Hit this";
     []
 
-and copy_choreo_expr (x : 'a Choreo.expr) (holder1 : string list) (_holder2 : string list)
+and visit_choreo_expr
+      (x : 'a Choreo.expr)
+      (holder1 : string list)
+      (_holder2 : string list)
   : string list
   =
   match x with
   | Unit _ -> holder1
   | Var (_, _) -> holder1
   | LocExpr (_, _, _) -> holder1
-  | Send (_, e, _, _) -> copy_choreo_expr e holder1 _holder2
+  | Send (_, e, _, _) -> visit_choreo_expr e holder1 _holder2
   | If (e1, e2, _, _) ->
     (* let list_three = *)
     (*   let list_two = *)
-    (*     let list_one = copy_choreo_expr e1 holder1 _holder2 in *)
-    (*     copy_choreo_expr e2 list_one _holder2 *)
+    (*     let list_one = visit_choreo_expr e1 holder1 _holder2 in *)
+    (*     visit_choreo_expr e2 list_one _holder2 *)
     (*   in *)
-    (*   copy_choreo_expr e3 list_two _holder2 *)
+    (*   visit_choreo_expr e3 list_two _holder2 *)
     (* in *)
     (* (\* let size = List.length list_three in *\) *)
     (* Printf.printf "This is the size of the internal list %d \n" size; *)
     (* list_three *)
-    let list_one = copy_choreo_expr e1 holder1 _holder2 in
-    copy_choreo_expr e2 list_one _holder2
+    let list_one = visit_choreo_expr e1 holder1 _holder2 in
+    visit_choreo_expr e2 list_one _holder2
   | Let (stmts, e, _) ->
     let list_one = collect_data stmts holder1 _holder2 in
-    copy_choreo_expr e list_one _holder2
-  | FunDef (_, e, _) -> copy_choreo_expr e holder1 _holder2
+    visit_choreo_expr e list_one _holder2
+  | FunDef (_, e, _) -> visit_choreo_expr e holder1 _holder2
   | FunApp (e1, e2, _) ->
-    let list_one = copy_choreo_expr e1 holder1 _holder2 in
-    copy_choreo_expr e2 list_one _holder2
+    let list_one = visit_choreo_expr e1 holder1 _holder2 in
+    visit_choreo_expr e2 list_one _holder2
   | Pair (e1, e2, _) ->
-    let list_one = copy_choreo_expr e1 holder1 _holder2 in
-    copy_choreo_expr e2 list_one _holder2
-  | Fst (e, _) -> copy_choreo_expr e holder1 _holder2
-  | Snd (e, _) -> copy_choreo_expr e holder1 _holder2
-  | Left (e, _) -> copy_choreo_expr e holder1 _holder2
-  | Right (e, _) -> copy_choreo_expr e holder1 _holder2
-  | Match (e, _, _) -> copy_choreo_expr e holder1 _holder2
+    let list_one = visit_choreo_expr e1 holder1 _holder2 in
+    visit_choreo_expr e2 list_one _holder2
+  | Fst (e, _) -> visit_choreo_expr e holder1 _holder2
+  | Snd (e, _) -> visit_choreo_expr e holder1 _holder2
+  | Left (e, _) -> visit_choreo_expr e holder1 _holder2
+  | Right (e, _) -> visit_choreo_expr e holder1 _holder2
+  | Match (e, _, _) -> visit_choreo_expr e holder1 _holder2
   | Sync (LocId (id1, _), _, LocId (id3, _), e, _) ->
     print_endline "Hit the Sync Ast node";
     let holder1 = List.cons id1 holder1 in
@@ -103,11 +106,12 @@ and copy_choreo_expr (x : 'a Choreo.expr) (holder1 : string list) (_holder2 : st
         | head :: tail -> if String.equal head acc then check_equality tail acc else false
       in
       let enable_opt = check_equality (List.tl result_list) (List.hd result_list) in
+      let final_list = List.cons (Bool.to_string enable_opt) final_list in
       if enable_opt
-      then print_endline "Optimization possible"
-      else
-        print_endline
-          "Optimization not possible because the sender for each of the control messages \
-           is different";
-      final_list)
+       then print_endline "Optimization possible"
+       else
+         print_endline
+           "Optimization not possible because the sender for each of the control \
+            messages is different";
+        final_list)
 ;;
