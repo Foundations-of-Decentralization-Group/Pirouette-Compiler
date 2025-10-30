a6538d5f7f0f1b7001f11fbcf6ca548f8a1d7b6a
zhartov <magusshulem@gmail.com>
2025-10-29 22:27:52 -0400

    More print statements to debug the sender location for the hashtable

--- lib/http_pirc/send_receive.ml
+++ lib/http_pirc/send_receive.ml
@@ -83,8 +83,9 @@ let handler _socket request body =
      | None ->
        Hashtbl.add message_queues unwrapped_sender_location (Eio.Stream.create 10);
        let indexed_queue = Hashtbl.find message_queues unwrapped_sender_location in
-       print_endline ("This value was put inside the queue : " ^ sender_body);
+       print_endline ("This value was put inside the queue : " ^ sender_body ^ " " ^ unwrapped_sender_location );
        Eio.traceln "%s" sender_body;
+       Eio.traceln "%s" unwrapped_sender_location;       
        Eio.Stream.add indexed_queue sender_body;
        Cohttp_eio.Server.respond_string
          ~status:`OK
